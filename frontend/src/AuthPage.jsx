fc9015bba3271b665832176bb5204e609dc3f31c
Richard Opiyo <richardoppiyo@gmail.com>
2023-04-27 11:24:49 +0300

    use git status

--- frontend/src/AuthPage.jsx
+++ frontend/src/AuthPage.jsx
@@ -1,9 +1,19 @@
 /* eslint-disable react/prop-types */
+import axios from 'axios';
 
 const AuthPage = (props) => {
     const onSubmit = (e) => {
       e.preventDefault();
       const { value } = e.target[0];
+
+      axios.post(
+        'http://localhost:3001/authenticate',
+        {username: value}
+      )
+      .then(r => props.onAuth({...r.data, secret: value}))
+      .catch(e => console.log(e))
+
+
       props.onAuth({ username: value, secret: value });
     };
   
