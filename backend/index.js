e7b9d1ac935da243a46b18603b29f823f30eb914
Richard Opiyo <richardoppiyo@gmail.com>
2023-04-27 10:27:07 +0300

    request for user creation

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const axios = require('axios');
 
 const app = express();
 app.use(express.json());
@@ -7,7 +8,17 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
-  return res.json({ username: username, secret: "sha256..." });
+try {
+  const r = await axios.post(
+    'https://api.chatengine.io/users/',
+    {username: username, first_name:username, secret: username },
+    {headers: { 'PRIVATE-KEY': '296f6f57-f7a9-41eb-95f3-1b00a8ea63cb'}}
+  );
+  return res.status(r.status).json(r.data);
+} catch (e) {
+  return res.status(e.response.status).json(e.response.data);
+}
+  // return res.json({ username: username, secret: "sha256..." });
 });
 
 app.listen(3001);
